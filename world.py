8a1600baf1f3052550dc5025f1c6af39b0071c4c
Balagovind <balagovind2094@gmail.com>
2018-09-07 17:14:39 +0530

    GREEN: Tests for rooms and exits successful.

--- world.py
+++ world.py
@@ -12,3 +12,25 @@ def start_position():
     start_pos = loaded_map["0"][0]
     return start_pos
 
+def if_exit(current_room, out_path):
+    load_file("./maps.json")
+    out = 0
+    out_str = "No exit that way"
+    for i in loaded_map[current_room][1:]:
+        if out_path not in i:
+            #print(len(loaded_map[current_room]))
+            #print(loaded_map[current_room][i])
+            out = 0
+            
+        elif out_path in i:
+            #print(loaded_map[current_room][i][1])
+            current_room = i[1]
+            out = 1
+            break
+        else:
+            return "ERROR"
+    if out == 0:
+        return out_str, current_room
+    elif out == 1:
+        return loaded_map[current_room][0], current_room
+           
